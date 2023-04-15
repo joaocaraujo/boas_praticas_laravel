d8d7d29ecaf0f58af75a9019014037c7ed46d94a
Joao Araujo <joaoluiz.araujo@meetplatform.com.br>
2023-04-15 14:37:38 -0300

    populating data in sqlite base for testing

---
+++ database/factories/OrderFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+use Faker\Generator as Faker;
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| This directory should contain each of the model factory definitions for
+| your application. Factories provide a convenient way to generate new
+| model instances for testing / seeding your application's database.
+|
+*/
+
+$factory->define(App\Models\Order::class, function (Faker $faker) {
+    return [
+        'status' => $faker->randomElement(['delivered', 'pending']),
+        'paid' => $faker->boolean(50),
+        'track_code' => md5(uniqid(rand(), true))
+    ];
+});
