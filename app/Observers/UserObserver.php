bddd7a6f1c071766f6962598c816e4068b2518e6
Joao Araujo <joaoluiz.araujo@meetplatform.com.br>
2023-04-17 08:25:41 -0300

    send email using Laravel Observer

---
+++ app/Observers/UserObserver.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Observers;
+
+use App\User;
+use Illuminate\Support\Facades\Mail;
+
+class UserObserver
+{
+    /**
+     * Handle the user "created" event.
+     *
+     * @param  \App\User  $user
+     * @return void
+     */
+    public function created(User $user)
+    {
+        Mail::send([], [], function ($message) {
+            $message->to('admin@site.com', 'Administrador')
+                ->subject("Cadastro de Usuario")
+                ->setBody("Um novo usuario foi cadastro com sucesso no sistema.");
+        });
+    }
+
+    /**
+     * Handle the user "updated" event.
+     *
+     * @param  \App\User  $user
+     * @return void
+     */
+    public function updated(User $user)
+    {
+        //
+    }
+
+    /**
+     * Handle the user "deleted" event.
+     *
+     * @param  \App\User  $user
+     * @return void
+     */
+    public function deleted(User $user)
+    {
+        //
+    }
+
+    /**
+     * Handle the user "restored" event.
+     *
+     * @param  \App\User  $user
+     * @return void
+     */
+    public function restored(User $user)
+    {
+        //
+    }
+
+    /**
+     * Handle the user "force deleted" event.
+     *
+     * @param  \App\User  $user
+     * @return void
+     */
+    public function forceDeleted(User $user)
+    {
+        //
+    }
+}
